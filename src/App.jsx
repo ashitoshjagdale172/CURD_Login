8fd38dabb91ea01e1767758709e641e5569c24e1
Shinde1214 <shindedatta2515@gmail.com>
2024-12-12 15:34:57 +0530

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import React from 'react'
 import RegistrationForm from './Components/RegistrationForm'
 
 import LoginPage from "./Components/Login_Page";
+import Navbar from './Components/Navbar';
 
 
 const App = () => {
   return (
     <div>
-
+      <Navbar/>
       <RegistrationForm/>
 
       <LoginPage />
